9a86315c7b859684c4d3477ff8e60f2d00d88648
guoyz2296-ux <guoyz2296@gmail.com>
2026-03-12 21:29:29 +0800

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -27,3 +27,19 @@ CREATE TABLE `health_records` (
     `record_time` DATETIME DEFAULT CURRENT_TIMESTAMP,
     INDEX `idx_patient_time` (`patient_id`, `record_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 4. 创建ai建议及背景长期储存表
+
+CREATE TABLE `ai_advice_logs` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `patient_id` BIGINT NOT NULL COMMENT '关联的患者ID',
+  `heart_rate_avg` INT DEFAULT NULL COMMENT '该阶段的平均心率',
+  `advice_content` TEXT NOT NULL COMMENT 'AI 生成的系统性建议',
+  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '建议生成时间',
+  PRIMARY KEY (`id`),
+  -- 建立索引，方便以后按患者 ID 快速查询历史建议
+  INDEX `idx_patient_id` (`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
